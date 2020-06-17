4448be56bf88e0704be50667f8568c8c9e94c03d
Henrique Chaves <henriquechavesmm@gmail.com>
2020-06-17 14:22:31 -0300

    Created HotelModel

--- server/resources/hotel.py
+++ server/resources/hotel.py
@@ -1,4 +1,5 @@
 from flask_restful import Resource, reqparse
+from models.hotel import HotelModel
 
 hoteisLista = [
     {
@@ -54,11 +55,8 @@ class Hotel(Resource):
     def post(self, hotel_id):
 
         dados = Hotel.argumentos.parse_args()
-
-        novo_hotel = {
-            'hotel_id': hotel_id,
-            **dados
-        }
+        hotel_objeto = HotelModel(hotel_id, **dados)
+        novo_hotel = hotel_objeto.json()
 
         hoteisLista.append(novo_hotel)
         return novo_hotel, 200
@@ -67,10 +65,8 @@ class Hotel(Resource):
 
         dados = Hotel.argumentos.parse_args()
 
-        novo_hotel = {
-            'hotel_id': hotel_id,
-            **dados
-        }
+        hotel_objeto = HotelModel(hotel_id, **dados)
+        novo_hotel = hotel_objeto.json()
 
         hotel = Hotel.find_hotel(hotel_id)
 
