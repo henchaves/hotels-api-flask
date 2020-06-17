b3fdcd4f1b6394106345d1a697ebe345e4573280
Henrique Chaves <henriquechavesmm@gmail.com>
2020-06-17 18:34:59 -0300

    Created db using SQLAlchemy

--- server/app.py
+++ server/app.py
@@ -1,12 +1,26 @@
 from flask import Flask
 from flask_restful import Api
 from resources.hotel import Hotel, Hoteis
+from sql_alchemy import banco
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
+
+
+@app.before_first_request
+def cria_banco():
+    banco.create_all()
+
 
 api.add_resource(Hoteis, '/hoteis/')
 api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
 
+banco.init_app(app)
+
+
 # if __name__ == "__main__":
 #   app.run(debug=True)
+# from sql_alchemy import banco
+# banco.init_app(app)
