ca7a122d416b2d6202ea79316fab749e015dcc50
Henrique Chaves <henriquechavesmm@gmail.com>
2020-06-18 16:00:15 -0300

    Updated PUT method from Hotel resource

--- server/resources/hotel.py
+++ server/resources/hotel.py
@@ -1,29 +1,29 @@
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
 
-hoteisLista = [
-    {
-        "hotel_id": "alpha",
-        "nome": "Alpha Hotel",
-        "estrelas": 4.3,
-        "diaria": 420.30,
-        "cidade": "Rio de Janeiro"
-    },
-    {
-        "hotel_id": "bravo",
-        "nome": "Bravo Hotel",
-        "estrelas": 3.7,
-        "diaria": 250.80,
-        "cidade": "Rio de Janeiro"
-    },
-    {
-        "hotel_id": "charlie",
-        "nome": "Charlie Hotel",
-        "estrelas": 4.7,
-        "diaria": 400.10,
-        "cidade": "Niterói"
-    }
-]
+# hoteisLista = [
+#     {
+#         "hotel_id": "alpha",
+#         "nome": "Alpha Hotel",
+#         "estrelas": 4.3,
+#         "diaria": 420.30,
+#         "cidade": "Rio de Janeiro"
+#     },
+#     {
+#         "hotel_id": "bravo",
+#         "nome": "Bravo Hotel",
+#         "estrelas": 3.7,
+#         "diaria": 250.80,
+#         "cidade": "Rio de Janeiro"
+#     },
+#     {
+#         "hotel_id": "charlie",
+#         "nome": "Charlie Hotel",
+#         "estrelas": 4.7,
+#         "diaria": 400.10,
+#         "cidade": "Niterói"
+#     }
+# ]
 
 
 class Hoteis(Resource):
@@ -56,15 +56,15 @@ class Hotel(Resource):
 
     def put(self, hotel_id):
         dados = Hotel.argumentos.parse_args()
-        hotel_objeto = HotelModel(hotel_id, **dados)
-        novo_hotel = hotel_objeto.json()
-        hotel = Hotel.find_hotel(hotel_id)
+        hotel = HotelModel.find_hotel(hotel_id)
         if hotel:
-            hotel.update(novo_hotel)
-            return novo_hotel, 200
+            hotel.update_hotel(**dados)
+            hotel.save_hotel()
+            return hotel.json(), 200
         else:
-            hoteisLista.append(novo_hotel)
-            return novo_hotel, 200
+            hotel = HotelModel(hotel_id, **dados)
+            hotel.save_hotel()
+            return hotel.json(), 201
 
     def delete(self, hotel_id):
         hotelToDelete = Hotel.find_hotel(hotel_id)
