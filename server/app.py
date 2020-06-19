935e8a0ed5c7f1e24bcf72e968970cab420facd2
Henrique Chaves <henriquechavesmm@gmail.com>
2020-06-19 18:31:11 -0300

    Implemented UserLogin resource

--- server/app.py
+++ server/app.py
@@ -1,13 +1,18 @@
 from flask import Flask
 from flask_restful import Api
 from resources.hotel import Hotel, Hoteis
-from resources.usuario import User, Users, UserRegister
+from resources.usuario import User, Users, UserRegister, UserLogin
+from flask_jwt_extended import JWTManager
 from sql_alchemy import banco
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'DontTellAnyone'
+
 api = Api(app)
+jwt = JWTManager(app)
 
 
 @app.before_first_request
@@ -20,6 +25,7 @@ api.add_resource(Hotel, '/hoteis/<string:hotel_id>/')
 api.add_resource(Users, '/usuarios/')
 api.add_resource(User, '/usuarios/<string:login>/')
 api.add_resource(UserRegister, '/cadastro/')
+api.add_resource(UserLogin, '/login/')
 
 banco.init_app(app)
 
--- server/resources/usuario.py
+++ server/resources/usuario.py
@@ -1,4 +1,6 @@
 from flask_restful import Resource, reqparse
+from flask_jwt_extended import create_access_token
+from werkzeug.security import safe_str_cmp
 from models.usuario import UserModel
 
 
@@ -47,3 +49,22 @@ class UserRegister(Resource):
             user = UserModel(**dados)
             user.save_user()
             return {"message": f"User '{dados['login']}' created successfully!"}, 201
+
+
+class UserLogin(Resource):
+
+    @classmethod
+    def post(cls):
+        atributos = reqparse.RequestParser()
+        atributos.add_argument('login', type=str, required=True,
+                               help="The field 'login' cannot be left blank.")
+        atributos.add_argument('senha', type=str, required=True,
+                               help="The field 'senha' cannot be left blank.")
+        dados = atributos.parse_args()
+        user = UserModel.find_user_by_login(dados['login'])
+        if user and safe_str_cmp(user.senha, dados['senha']):
+            token_de_acesso = create_access_token(identity=user.user_id)
+            return {"access_token": token_de_acesso}, 200
+        else:
+            # Unauthorized
+            return {"message": "The username or password is incorrect."}, 401
