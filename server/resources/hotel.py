22922db6f75a80577f670b52aec42b0a678ec20b
Henrique Chaves <henriquechavesmm@gmail.com>
2020-06-20 00:06:03 -0300

    FINISHED HOTELS REST API

--- server/resources/hotel.py
+++ server/resources/hotel.py
@@ -1,31 +1,6 @@
 from flask_restful import Resource, reqparse
 from flask_jwt_extended import jwt_required
 from models.hotel import HotelModel
-#import sqlite3
-
-# hoteisLista = [
-#     {
-#         "hotel_id": "alpha",
-#         "nome": "Alpha Hotel",
-#         "estrelas": 4.3,
-#         "diaria": 420.30,
-#         "cidade": "Rio de Janeiro"
-#     },
-#     {
-#         "hotel_id": "bravo",
-#         "nome": "Bravo Hotel",
-#         "estrelas": 3.7,
-#         "diaria": 250.80,
-#         "cidade": "Rio de Janeiro"
-#     },
-#     {
-#         "hotel_id": "charlie",
-#         "nome": "Charlie Hotel",
-#         "estrelas": 4.7,
-#         "diaria": 400.10,
-#         "cidade": "Niterói"
-#     }
-# ]
 
 
 def normalize_path_params(cidade=None, estrelas_min=0, estrelas_max=5, diaria_min=0, diaria_max=10000, limit=50, offset=0, **dados):
