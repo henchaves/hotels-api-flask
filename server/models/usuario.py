b629a661f8f0f82c71c608abdcff09edb10ae6c1
Henrique Chaves <henriquechavesmm@gmail.com>
2020-06-24 11:48:14 -0300

    Using yagmail instead of mailgun

--- server/models/usuario.py
+++ server/models/usuario.py
@@ -1,6 +1,8 @@
 from sql_alchemy import banco
 from flask import request, url_for
 from requests import post
+import yagmail
+from email_credentials import EMAIL, PASSWORD
 
 MAILGUN_DOMAIN = "sandbox7678d1582790407a9948f037fc15b994.mailgun.org"
 MAILGUN_API_KEY = "595e7234581cbab54c9d6d6a04a718f0-468bde97-270ab189"
@@ -14,7 +16,7 @@ class UserModel(banco.Model):
     user_id = banco.Column(banco.Integer, primary_key=True)
     nome = banco.Column(banco.String(100))
     login = banco.Column(banco.String(40), nullable=False, unique=True)
-    email = banco.Column(banco.String(80), nullable=False, unique=True)
+    email = banco.Column(banco.String(80), nullable=False)
     senha = banco.Column(banco.String(40), nullable=False)
     ativado = banco.Column(banco.Boolean, default=False)
 
@@ -34,19 +36,26 @@ class UserModel(banco.Model):
             "ativado": self.ativado
         }
 
+    # def send_confirmation_email(self):
+    #     link = request.url_root[:-1] + \
+    #         url_for('userconfirm', user_id=self.user_id)
+    #     return post(f"https://api.mailgun.net/v3/{MAILGUN_DOMAIN}/messages",
+    #                 auth=('api', MAILGUN_API_KEY),
+    #                 data={'from': f"{FROM_TITLE} <{FROM_EMAIL}>",
+    #                       "to": self.email,
+    #                       "subject": "Confirmação de cadastro",
+    #                       "text": f"Confirme seu cadastro clicando no link a seguir: {link}",
+    #                       "html": f"""<html>
+    #                                 <p>Confirme seu cadastro clicando no link a seguir: <a href="{link}">CONFIRMAR E-MAIL</a></p>
+    #                               </html>
+    #                            """})
     def send_confirmation_email(self):
+        yag = yagmail.SMTP(EMAIL, PASSWORD)
         link = request.url_root[:-1] + \
             url_for('userconfirm', user_id=self.user_id)
-        return post(f"https://api.mailgun.net/v3/{MAILGUN_DOMAIN}/messages",
-                    auth=('api', MAILGUN_API_KEY),
-                    data={'from': f"{FROM_TITLE} <{FROM_EMAIL}>",
-                          "to": self.email,
-                          "subject": "Confirmação de cadastro",
-                          "text": f"Confirme seu cadastro clicando no link a seguir: {link}",
-                          "html": f"""<html>
-                                    <p>Confirme seu cadastro clicando no link a seguir: <a href="{link}">CONFIRMAR E-MAIL</a></p>
-                                  </html>
-                               """})
+        contents = [
+            f"Confirme seu cadastro clicando no link a seguir: {link}"]
+        yag.send(self.email, 'NO-REPLY: Confirmação de Cadastro', contents)
 
     @classmethod
     def find_user_by_id(cls, user_id):
